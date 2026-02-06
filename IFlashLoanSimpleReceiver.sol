91c52d8ae5dede8e97de6fc3605b4aa2095e001d
Sirajul <161524666+sirajul9988@users.noreply.github.com>
2026-02-06 11:21:15 +0600

    Create IFlashLoanSimpleReceiver.sol

---
+++ IFlashLoanSimpleReceiver.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IFlashLoanSimpleReceiver {
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool);
+}
